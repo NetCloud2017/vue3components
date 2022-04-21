dd0c75b250cd230ca7c93d1c770ebf98b44eff23
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2022-04-21 17:57:07 +0800

    创建组合键种类

--- src/main.js
+++ src/main.js
@@ -11,14 +11,35 @@ app.mount("#app");
 document.addEventListener(
   "keydown",
   function (event) {
-    let { key } = event;
+    let { key, ctrlKey, shiftKey, altKey } = event;
     key = `${key}`.toLowerCase();
-    // control 组合键 操作
-    if (event.ctrlKey) {
-      let func = actions["control"] && actions["control"][key];
+    console.log(event);
+
+    let keyActionClass = "";
+
+    if (ctrlKey || shiftKey || altKey) {
+      // 创建组合键种类
+      ctrlKey && (keyActionClass = "control");
+      shiftKey && (keyActionClass = "shift");
+      altKey && (keyActionClass = "alt");
+      if (ctrlKey && shiftKey) {
+        keyActionClass = "ctrl&shift";
+      }
+      if (ctrlKey && altKey) {
+        keyActionClass = "ctrl&alt";
+      }
+      if (shiftKey && altKey) {
+        keyActionClass = "shift&alt";
+      }
+    }
+    const keyActions = actions[keyActionClass];
+    if (keyActions) {
+      // 获取 组合键 操作
+      let func = keyActions[key];
       func && func();
     }
-   // 阻止 浏览器 快捷键 默认事件 
+
+    // 阻止 浏览器 快捷键 默认事件
     if (event.preventDefault) {
       event.stopPropagation();
       event.preventDefault();
