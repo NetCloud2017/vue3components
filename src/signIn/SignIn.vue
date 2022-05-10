2b5f2e2c0028d846c01563da19a5e0b961d1ebcf
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2022-05-10 11:57:54 +0800

    madd form

--- src/signIn/SignIn.vue
+++ src/signIn/SignIn.vue
@@ -1,49 +1,45 @@
 <template>
   <div class="loginPage">
-    <a-form>
+    <a-form :data="form">
       <a-input @blur="onBlur" v-model:value="phoneNo" />
       <a-input v-model:value="email" />
       <a-input v-model:value="password" />
       <a-input v-model:value="userName" />
+      <a-input v-model:value="emailAddress" />
+      <a-input v-model:value="verificationCode" />
+      <!-- <a-input v-model:value="agreeContract" /> -->
+      
     </a-form>
+    
   </div>
 </template>
 
-<script>
-export default {
-  name: "sign-in",
-  data: function () {
-    return {
-      userName: "",
-      password: "",
-      email: "",
-      phoneNo: "",
-    };
-  },
+<script setup>
+import { reactive } from "vue";
 
-  mounted() {
-    console.log(this, "uux");
-  },
-  beforeUpdate() {
-    console.log(this.userName, 'uunnnnn');
-  },
-  methods: {
-    onBlur(val) {
-      console.log("2323", val);
-    },
-  },
-};
+// do not use same name with ref
+const form = reactive({
+  userName: "",
+  password: "",
+  emailAddress: "",
+  phoneNo: "",
+  verificationCode: "",
+  agreeContract: false,
+});
+
+// const onSubmit = () => {
+//   console.log("submit!");
+// };
 </script>
-
 <style lang="less" scoped>
-  .loginPage {
-    width: 100%;
-    height: 100vh;
-    background-image: url('../assets/imgs/futures_login_backgroundImg.jpeg');
-    background-size: 100%;
-    background-repeat: no-repeat;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-</style>> 
+.loginPage {
+  width: 100%;
+  height: 100vh;
+  background-image: url("../assets/imgs/futures_login_backgroundImg.jpeg");
+  background-size: 100%;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
