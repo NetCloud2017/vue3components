33f9093b9ceb8fa3e72aff95a6e61b12085fc506
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2022-06-23 23:50:37 +0800

    m修改 form 参数

--- src/signIn/SignIn.vue
+++ src/signIn/SignIn.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="loginPage">
-    <a-form :data="form">
-      <form-item :label="'用户名'">
-        <a-input v-model:value="userName" />
-      </form-item>
-      <form-item :label="'密码'">
-        <a-input v-model:value="password" />
-      </form-item>
-      <form-item :label="'电话号码'">
-        <a-input @blur="onBlur" v-model:value="phoneNo" />
-      </form-item>
+    <div class="loginPage">
+        <a-form :data="form">
+            <form-item :label="'用户名'">
+                <a-input v-model:value="form.userName" />
+            </form-item>
+            <form-item :label="'密码'">
+                <a-input v-model:value="form.password" />
+            </form-item>
+            <form-item :label="'电话号码'">
+                <a-input @blur="onBlur" v-model:value="form.phoneNo" />
+            </form-item>
 
-      <form-item :label="'邮箱地址'">
-        <a-input v-model:value="emailAddress" />
-      </form-item>
-      <form-item :label="'验证码'">
-        <a-input v-model:value="verificationCode" />
-      </form-item>
-      <form-item :label="'用户协议'">
-        <a-input v-model:value="agreeContract" />
-      </form-item>
-    </a-form>
-  </div>
+            <form-item :label="'验证码'">
+                <a-input v-model:value="form.verificationCode" />
+            </form-item>
+            <form-item>
+                <el-checkbox v-model="form.agreeContract" />
+                <el-button class="contractText" type="text" @click="showContract = true">
+                    用户协议
+                </el-button>
+            </form-item>
+        </a-form>
+    </div>
 </template>
 
 <script setup>
@@ -29,12 +29,12 @@ import { reactive } from "vue";
 
 // do not use same name with ref
 const form = reactive({
-  userName: "",
-  password: "",
-  emailAddress: "",
-  phoneNo: "",
-  verificationCode: "",
-  agreeContract: false,
+    userName: "",
+    password: "",
+    emailAddress: "",
+    phoneNo: "",
+    verificationCode: "",
+    agreeContract: false,
 });
 
 // const onSubmit = () => {
@@ -43,13 +43,16 @@ const form = reactive({
 </script>
 <style lang="less" scoped>
 .loginPage {
-  width: 100%;
-  height: 100vh;
-  background-image: url("../assets/imgs/futures_login_backgroundImg.jpeg");
-  background-size:  100% 100%;
-  background-repeat: no-repeat;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    width: 100%;
+    height: 100vh;
+    background-image: url("../assets/imgs/futures_login_backgroundImg.jpeg");
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.contractText {
+    margin-left: 10px;
 }
 </style>
