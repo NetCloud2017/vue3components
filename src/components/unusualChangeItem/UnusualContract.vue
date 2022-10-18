549467f44b6d44075956d0b205c20e8b6bf272a4
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2022-10-18 22:10:10 +0800

    unusualcontracl 更改为 Vue 3 组件。

--- src/components/unusualChangeItem/UnusualContract.vue
+++ src/components/unusualChangeItem/UnusualContract.vue
@@ -1,11 +1,91 @@
+<script>
+import * as Echarts from "echarts";
+import { fetchData } from "../../api";
+import { defineComponent, onMounted, ref, nextTick } from "vue";
+
+let myChart = null;
+export default defineComponent({
+    name: "unusual-contract",
+    props: {
+        contract: {
+            type: Object,
+        },
+    },
+    setup() {
+        let quotationMap = ref(null);
+        onMounted(() => {
+            fetchData({
+                url: "futures_zh_spot",
+                data: {
+                    subscribe_list: "P2209",
+                    market: "CF",
+                    adjust: "0",
+                },
+            }).then((res) => {
+                console.log(res, "dat222a");
+            });
+            nextTick(() => {
+                myChart = Echarts.init(quotationMap.value);
+                myChart.setOption({
+                    backgroundColor: "rgba(255,255,255, 0.3)",
+                    grid: {
+                        //  xy 轴位置
+                        left: 0,
+                        // right: 0,
+                        // height: "100%",
+                        bottom: 0,
+                    },
+                    xAxis: {
+                        type: "category",
+                        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+                        show: false,
+                        boundaryGap: false,
+                    },
+                    yAxis: {
+                        type: "value",
+                        splitLine: false,
+                        min: 0,
+
+                        max: 1330,
+                        show: false,
+                        left: 0,
+                    },
+                    series: [
+                        {
+                            symbol: "none", // 去除 横向线。
+                            data: [820, 932, 901, 934, 1290, 1330, 1320],
+                            type: "line",
+                            smooth: true,
+                            itemStyle: {
+                                normal: {
+                                    lineStyle: {
+                                        width: 0.5, // 0.1的线条是非常细的了
+                                        color: "red",
+                                    },
+                                },
+                            },
+                        },
+                    ],
+                });
+            });
+        });
+
+        return {
+            quotationMap,
+        };
+    },
+});
+</script>
+
 <template>
     <div class="item">
         <div class="contractInfo">
             <div class="contractTitle">
                 {{ contract.contractName }}
+                <span class="futuresCode">{{ "p2209" }}</span>
             </div>
             <span :class="['percent', contract.riseAndFall]">
-                {{ contract.contractPercent }}
+                {{ `${contract.contractPercent} %` }}
             </span>
             <p :class="['currentPrice', contract.riseAndFall]">
                 {{ contract.contractPrice }}
@@ -15,85 +95,14 @@
     </div>
 </template>
 
-<script>
-import * as Echarts from "echarts";
-import { fetchData } from "../../api";
-let myChart = null;
-export default {
-    name: "unusual-contract",
-    props: {
-        contract: {
-            type: Object,
-        },
-    },
-    setup() {},
-    mounted() {
-        fetchData({
-            url: "futures_zh_spot",
-            data: {
-                subscribe_list: "P2209",
-                market: "CF",
-                adjust: "0",
-            },
-        }).then((res) => {
-            console.log(res, "dat222a");
-        });
-        this.$nextTick(() => {
-            myChart = Echarts.init(this.$refs.quotationMap);
-            myChart.setOption({
-                backgroundColor: "rgba(255,255,255, 0.3)",
-                grid: {
-                    //  xy 轴位置
-                    left: 0,
-                    // right: 0,
-                    // height: "100%",
-                    bottom: 0,
-                },
-                xAxis: {
-                    type: "category",
-                    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-                    show: false,
-                    boundaryGap: false,
-                },
-                yAxis: {
-                    type: "value",
-                    splitLine: false,
-                    min: 0,
-
-                    max: 1330,
-                    show: false,
-                    left: 0,
-                },
-                series: [
-                    {
-                        symbol: "none", // 去除 横向线。
-                        data: [820, 932, 901, 934, 1290, 1330, 1320],
-                        type: "line",
-                        smooth: true,
-                        itemStyle: {
-                            normal: {
-                                lineStyle: {
-                                    width: 0.5, // 0.1的线条是非常细的了
-                                    color: "red",
-                                },
-                            },
-                        },
-                    },
-                ],
-            });
-        });
-    },
-};
-</script>
-
 <style lang="less">
 .item {
     width: 100%;
     height: 100px;
     display: flex;
     align-items: center;
-    // padding: 8px 5px;
     text-align: center;
+    margin-bottom: 10px;
     .contractInfo {
         color: #fff;
         min-width: 110px;
@@ -102,6 +111,9 @@ export default {
             font-weight: bold;
             line-height: 16px;
             margin-bottom: 5px;
+            .futuresCode {
+                font-size: 12px;
+            }
         }
         .percent {
             font-size: 15px;
